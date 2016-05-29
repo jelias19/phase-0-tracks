4f38c5e8a8c23287e569b4e37cd9c94c67b17d9b
Jeffrey Elias <elias.jeffrey@gmail.com>
2016-05-29 12:02:52 -0400

    Completed questioning in Release 1.

---
+++ ruby/vampires.rb
@@ -0,0 +1,15 @@
+puts "What is your name?"
+	name = gets.chomp
+
+puts "How old are you?"
+	age = gets.chomp
+
+puts "What year were you born?"
+	year_born = gets.chomp
+
+puts "Our cafeteria serves delicious garlic bread. Would you us to order some for you?"
+	garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company's health insurance? There are no additional charges!"
+	health_insurance = gets.chomp
+
