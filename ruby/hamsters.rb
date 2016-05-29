1883aa49244c4472b2e63f7193f71034abb18747
Jeffrey Elias <elias.jeffrey@gmail.com>
2016-05-29 11:19:49 -0400

    Cleaned up printing results based on variable information.

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -8,21 +8,24 @@ puts "What is the color of the hamsters fur?"
 	fur_color = gets.chomp
 
 puts "Would you adopt this hamster? Yes, no, or maybe?"
-	adopt = gets.chomp
+	adopt = gets.chomp.downcase
 
 puts "What is the approximate age of this hamster?"
-	age = gets.chomp.to_f
-	if age == " "
+	age = gets.chomp
+	if age == ""
 		age = "nil"
 	else 
 		age = age.to_f
 	end
+	
 
-puts "The hamster is named #{name}, and is #{volume} loud on a scale of 1-10."
-puts "The hamster is #{fur_color} and approximately #{age}. years old."
+puts "The hamster is named #{name}, and is #{volume} notches loud on a scale of 1-10."
+puts "The hamster is #{fur_color} and approximately #{age} years old."
 
-if adopt == "yes" || "maybe"
-	puts "#{adopt}, I would adopt this hamster."
+if adopt == "yes"
+	puts "#{adopt.capitalize}, I would adopt this hamster."
+elsif adopt == "maybe"
+	puts "#{adopt.capitalize}, I would adopt this hamster."
 else adopt == "no"
-	puts "#{adopt}, I would not adopt this hamster."
+	puts "#{adopt.capitalize}, I would not adopt this hamster."
 end
