1d9623a8ca76f853f44534a909349c3e8499ab6c
Jeffrey Elias <elias.jeffrey@gmail.com>
2016-05-31 22:06:31 -0400

    Method detective completed.

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,39 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".swapcase
+p "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".insert(1, 'o')
+p "zom".insert(1, 'o')
 # => “zoom”
 
-# "enhance".center(15)
+p "enhance".center(15)
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".upcase
+p "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".insert 9, " suspects"
+p "the usual".insert 9, " suspects"
 #=> "the usual suspects"
 
-# " suspects".insert 0, "the usual"
+p " suspects".insert 0, "the usual"
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".chop
+p "The case of the disappearing last letter".chop
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+letter = "The mystery of the missing first letter"
+letter.slice!(0)
+p letter
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".split.join (" ")
+p "Elementary,    my   dear        Watson!".split.join (" ")
 # => "Elementary, my dear Watson!"
 
-# "z".ord
+p "z".ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+#This method is for compatiability to characters contant in ASC.
 
-# "How many times does the letter 'a' appear in this string?".count "a"
+p "How many times does the letter 'a' appear in this string?".count "a"
 # => 4
