4bc3fb628c5d6411b401332430d839fde8b33b8b
Jeffrey Elias <elias.jeffrey@gmail.com>
2016-06-04 18:07:06 -0400

    Created a method to swap first name with last name. Built a user interface that will keep running method until quit entered.

---
+++ ruby/alias_manager.rb
@@ -0,0 +1,20 @@
+#Build a method that creates a fake name given a spy's real name.
+#Take Spy name as input
+#Swap spy's first name with last name, creating an array
+#Change all vowels to next vowel, and all consonants to next consonant. 
+
+def first_last_swap(name)
+	name.split(' ').reverse.join(' ')
+end
+
+
+#Creating a User Interface
+username = ""
+
+#Ask user for information
+#Continue to ask until quit is entered.
+while username!= "quit"
+puts "What name would you like to be aliased? Type quit when done."
+	username = gets.chomp
+	p first_last_swap(username)
+end
