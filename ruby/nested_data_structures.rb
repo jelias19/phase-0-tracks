14650f8a03c371c09c8e691a0ddc772bc176c5ee
Jeffrey Elias <elias.jeffrey@gmail.com>
2016-06-04 16:02:04 -0400

    Built out nested data structure relating to a hockey rink.

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,99 @@
+hockey_stadium = {
+	ice_rink: {
+		name: 'Buffalo Wings',
+		sponsors: [
+			"Bauer",
+			"Reebok",
+			"EA Sports"
+			]
+		capacity: {
+			seating_room: 400,
+			standing_room: 100;
+		}
+	bathroom_1: {
+		gender: 'Male',
+		sinks: 6,
+		toilets:{
+			urinals: 6,
+			stalls: 3,
+		}
+	}
+	bathroom_2:{
+		gender: 'Female',
+		sinks: 6,
+		toilets:{
+			urinals: 0,
+			stalls: 8,
+	}
+	locker_room_1:{
+		name: 'Guest Room 1',
+		showers: 2,
+		sinks: 2,
+		toilets:{
+			urinals: 2,
+			stalls: 1,
+		}
+		seating: 18
+	}
+	locker_room_2:{
+		name: 'Guest Room 2',
+		showers: 2,
+		sinks: 2,
+		toilets:{
+			urinals: 2,
+			stalls: 1,
+		}
+		seating: 14
+	}
+	locker_room_3:{
+		name: 'Guest Room 3'
+		showers: 0,
+		sinks: 2,
+		toilets:{
+			urinals: 1,
+			stalls: 0,
+		}
+		seating: 14
+	}
+	home_team_locker_room: {
+		name: 'Buffalo Wings Den',
+		showers: 4,
+		sinks: 4,
+		toilets:{
+			urinals: 3,
+			stalls: 2,
+		}
+		seating: 24,
+	}
+	cafeteria:{
+		Drinks: [
+			"Gatorade",
+			"Snapple",
+			"Water",
+			Soda:[ 
+				"Pepsi",
+				"Diet Pepsi",
+				"Mountain Dew"
+			]
+		]
+		Snacks: [
+			"Popcorn",
+			"Nachos",
+			Candy:[
+				"Twix",
+				"Snickers"
+				"Swedish Fish"
+			]
+		]
+		Entrees: [
+			"Cheeseburger",
+			"Hamburger",
+			"Chicken Fingers with Fries",
+			"Hot Dog",
+		]
+		Condiments: [
+			"Ketchup",
+			"Mustard",
+			"Mayonanise"]
+	}
+}
