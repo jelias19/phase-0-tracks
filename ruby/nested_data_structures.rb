a60ef5bd544b56b8370fd13786cfc068b452ecad
Jeffrey Elias <elias.jeffrey@gmail.com>
2016-06-04 16:09:54 -0400

    Fixed errors in data structure.

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -6,55 +6,57 @@ hockey_stadium = {
 			"Reebok",
 			"EA Sports"
 			]
+	},
 		capacity: {
 			seating_room: 400,
-			standing_room: 100;
-		}
+			standing_room: 100
+		},
 	bathroom_1: {
 		gender: 'Male',
 		sinks: 6,
-		toilets:{
+		toilets: {
 			urinals: 6,
-			stalls: 3,
-		}
-	}
-	bathroom_2:{
+			stalls: 3
+		},
+	},
+	bathroom_2: {
 		gender: 'Female',
 		sinks: 6,
 		toilets:{
 			urinals: 0,
-			stalls: 8,
-	}
-	locker_room_1:{
+			stalls: 8
+	},
+	},
+	locker_room_1: {
 		name: 'Guest Room 1',
 		showers: 2,
 		sinks: 2,
 		toilets:{
 			urinals: 2,
 			stalls: 1,
-		}
+		},
 		seating: 18
-	}
-	locker_room_2:{
+	},
+	locker_room_2: {
 		name: 'Guest Room 2',
 		showers: 2,
 		sinks: 2,
 		toilets:{
 			urinals: 2,
 			stalls: 1,
-		}
+		},
 		seating: 14
-	}
-	locker_room_3:{
-		name: 'Guest Room 3'
+	},
+	locker_room_3: {
+		name: 'Guest Room 3',
 		showers: 0,
 		sinks: 2,
 		toilets:{
 			urinals: 1,
 			stalls: 0,
-		}
+		},
 		seating: 14
-	}
+	},
 	home_team_locker_room: {
 		name: 'Buffalo Wings Den',
 		showers: 4,
@@ -62,9 +64,9 @@ hockey_stadium = {
 		toilets:{
 			urinals: 3,
 			stalls: 2,
-		}
+		},
 		seating: 24,
-	}
+	},
 	cafeteria:{
 		Drinks: [
 			"Gatorade",
@@ -74,26 +76,26 @@ hockey_stadium = {
 				"Pepsi",
 				"Diet Pepsi",
 				"Mountain Dew"
-			]
-		]
+			],
+		],
 		Snacks: [
 			"Popcorn",
 			"Nachos",
 			Candy:[
 				"Twix",
-				"Snickers"
+				"Snickers",
 				"Swedish Fish"
-			]
-		]
+			],
+		],
 		Entrees: [
 			"Cheeseburger",
 			"Hamburger",
 			"Chicken Fingers with Fries",
 			"Hot Dog",
-		]
+		],
 		Condiments: [
 			"Ketchup",
 			"Mustard",
-			"Mayonanise"]
-	}
+			"Mayonanise"],
+	},
 }
