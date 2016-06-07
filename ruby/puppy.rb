bde21348b474fc98b76d2eccec5a9d4a7708c01e
Jeffrey Elias <elias.jeffrey@gmail.com>
2016-06-06 21:21:12 -0400

    Added characteristics and behaviour for puppy.

---
+++ ruby/puppy.rb
@@ -0,0 +1,19 @@
+# SPECIES ------------------------
+# Furrious Playfully
+
+# CHARACTERISTICS ----------------
+# Legs : 4
+# Eye count: 2
+# Fur length: varies
+# Friendly: yes
+# Name: varies
+# Color: varies
+# Bark volume : varies
+
+
+# BEHAVIOR -----------------------
+# Jump
+# Run
+# Play
+# Eat
+# Sleep
