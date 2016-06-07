0eed0f5b4a202687d7cebe7cba5e6f503aa4f3d9
Jeffrey Elias <elias.jeffrey@gmail.com>
2016-06-06 22:00:25 -0400

    Established Santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,24 +1,23 @@
 # SANTACON SIMULATOR
 
-
-
 # BUSINESS LOGIC
 
-
-# Method to print santas message
-def speak
+class Santa
+	# Method to print santas message
+	def speak
 	p "Ho, ho, ho! Haaaappy holidays!"
-end
+	end
 
-# Method that takes a cookie type and print result
-def eat_milk_and_cookies
-#Ask user for cookie type
-	puts "What cookie would you like?"	
-	cookie_type = gets.chomp
-	puts "That was a good #{cookie_type}."
-end
+	# Method that takes a cookie type and print result
+	def eat_milk_and_cookies
+	#Ask user for cookie type
+		puts "What cookie would you like?"	
+		cookie_type = gets.chomp
+		puts "That was a good #{cookie_type}."
+	end
 
-# Method that prints "Initializing Santa instance"
-def initialize
-	puts "Initializing Santa instance..."
+	# Method that prints "Initializing Santa instance"
+	def initialize
+		puts "Initializing Santa instance..."
+	end
 end
