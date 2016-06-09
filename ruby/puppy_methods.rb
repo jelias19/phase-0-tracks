fc33b2d272c82568729579a3d9319602b7c8e418
Jeffrey Elias <elias.jeffrey@gmail.com>
2016-06-08 23:21:27 -0400

    Replicate file that I completed with TJ. Puppy methods, created own class with two instance methods.

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,9 +1,69 @@
 class Puppy
 
+  def initialize
+    puts "initializing new puppy instance..."
+  end
+
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
 
+  def speak(num)
+    num.times {puts "Woof"}
+  end
+
+  def rollover
+    puts "*Rolls over*"
+  end
+
+  def dog_years(num_of_human_years)
+    puts "Dog years is: #{num_of_human_years * 7}."
+  end
+
+  def owners(name)
+    puts "My owner is #{name} "
+  end
 end
 
+#DRIVER CODE PUPPY
+
+=begin
+Tom = Puppy.new
+Tom.fetch("game")
+Tom.speak(3)
+Tom.rollover
+Tom.dog_years(5)
+Tom.owners("Tj")
+=end
+
+#Friend Class
+class Friend
+
+# Initialize method
+def initialize
+  puts "Initializing new Friend instance..."
+end
+
+def say_hi(name)
+  puts "TJ says hi to #{name}."
+end
+
+def say_bye(num_of_waves)
+  num_of_waves.times {puts "TJ waves goodbye #{num_of_waves} times."}
+end
+
+end
+
+TJ = Friend.new
+TJ.say_hi("Jeff")
+TJ.say_bye(3)
+
+clones_of_TJ = []
+50.times {clones_of_TJ << Friend.new}
+p clones_of_TJ
+
+clones_of_TJ.each {|x| 
+  puts x.say_hi("Bob")
+  puts x.say_bye(2)
+}
