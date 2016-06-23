595380fd9d6895baa77ac31d1e424a9aaa406802
Jeffrey Elias <elias.jeffrey@gmail.com>
2016-06-23 18:54:57 -0400

    Initial pseudocode for tryout application. Set layout for what needs to be done for database and basic idea..

---
+++ SQL_Ruby/tryouts.rb
@@ -0,0 +1,15 @@
+# Build a program that allows user to enter information for players 
+# trying out for hockey team
+# Player information will include
+# 		name, jersey number, position, age, previous team
+
+# Require gems
+
+# Create a SQLite3 Database
+
+# Create a table of players
+
+# Add initial player
+
+# Build a user interface that asks to enter each players information
+# Continue add players until user 'quits'
