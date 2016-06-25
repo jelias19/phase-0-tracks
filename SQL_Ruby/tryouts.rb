923286143e1b994e71968f6fbc7fb818257a684b
Jeffrey Elias <elias.jeffrey@gmail.com>
2016-06-25 10:03:16 -0400

    Created table for hockey player tryout, tested adding one player to database.

--- SQL_Ruby/tryouts.rb
+++ SQL_Ruby/tryouts.rb
@@ -9,9 +9,29 @@ require 'sqlite3'
 # Create a SQLite3 Database
 tryout_roster = SQLite3::Database.new("players.db")
 
+create_table_cmd = <<-BING
+	CREATE TABLE IF NOT EXISTS tryout_roster(
+		id INTEGER PRIMARY KEY,
+		name VARCHAR(255),
+		age INT,
+		position VARCHAR(255),
+		hand VARCHAR(255)
+	)
+BING
+
 # Create a table of players
+tryout_roster.execute(create_table_cmd)
 
 # Add initial player
+tryout_roster.execute("INSERT INTO tryout_roster (name, age, position, hand) VALUES ('Jeff Elias', 29, 'Center', 'Right')")
+
 
 # Build a user interface that asks to enter each players information
 # Continue add players until user 'quits'
+
+
+
+
+
+
+
