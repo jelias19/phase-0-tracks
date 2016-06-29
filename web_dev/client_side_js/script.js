5e3d9021421efee02c3d30637cf509adcb0e69b0
Jeffrey Elias <elias.jeffrey@gmail.com>
2016-06-28 20:46:08 -0400

    Built JS functions to hide and reveal photo.

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -3,3 +3,19 @@ console.log(a);
 
 var photo = document.getElementById("coding");
 photo.style.width="500px"
+
+function makeInvisible(event){
+	console.log("make it" + event.target + "make invisible");
+	console.log(event.target);
+	event.target.style.visibility="hidden";
+}
+
+function makeVisible(event){
+	event.target.style.visibility="visible";
+}
+
+var body = document.getElementById("page");
+
+body.addEventListener("mouseover", makeInvisible);
+body.addEventListener("mouseout", makeVisible);
+
